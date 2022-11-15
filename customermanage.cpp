92f83476602fd7fbaa96a932f4cb5771b0946191
GlennPark <downgear@naver.com>
2022-11-15 20:58:09 +0900

    1115_02
    CM 주석작업

--- customermanage.cpp
+++ customermanage.cpp
@@ -20,6 +20,7 @@ CustomerManage::CustomerManage(QWidget* parent)
 {
     ui->setupUi(this);
 
+    // 우클릭 후 삭제 시 ROW 에 포함된 내용들을 지우는 액션
     QAction* removeAction = new QAction(tr("&Remove"));
     connect(removeAction, SIGNAL(triggered()), this, SLOT(removeItem()));
     menu = new QMenu;
@@ -33,33 +34,38 @@ CustomerManage::CustomerManage(QWidget* parent)
     // 나이 입력 시 제한
     ui->ageSpinBox->setRange(19,100);
 
-
+    // 검색 시 보여주는 항목들을 seachTreeView 의 헤더로 설정
     sModel = new QStandardItemModel(0, 10);
-    sModel->setHeaderData(0, Qt::Horizontal, tr("CID"));
-    sModel->setHeaderData(1, Qt::Horizontal, tr("NAME"));
-    sModel->setHeaderData(2, Qt::Horizontal, tr("PHONE NUMBER"));
-    sModel->setHeaderData(3, Qt::Horizontal, tr("EMAIL"));
-    sModel->setHeaderData(4, Qt::Horizontal, tr("DOMAIN"));
-    sModel->setHeaderData(5, Qt::Horizontal, tr("ADDRESS"));
-    sModel->setHeaderData(6, Qt::Horizontal, tr("FAVORITE"));
-    sModel->setHeaderData(7, Qt::Horizontal, tr("AGE"));
-    sModel->setHeaderData(8, Qt::Horizontal, tr("GENDER"));
-    sModel->setHeaderData(9, Qt::Horizontal, tr("JOINDATE"));
+    sModel->setHeaderData(0, Qt::Horizontal, tr("CID"));                // 회원 Key
+    sModel->setHeaderData(1, Qt::Horizontal, tr("NAME"));               // 회원 이름
+    sModel->setHeaderData(2, Qt::Horizontal, tr("PHONE NUMBER"));       // 전화 번호
+    sModel->setHeaderData(3, Qt::Horizontal, tr("EMAIL"));              // 이메일 주소
+    sModel->setHeaderData(4, Qt::Horizontal, tr("DOMAIN"));             // 도메인 주소
+    sModel->setHeaderData(5, Qt::Horizontal, tr("ADDRESS"));            // 거주지 주소
+    sModel->setHeaderData(6, Qt::Horizontal, tr("FAVORITE"));           // 선호 항목
+    sModel->setHeaderData(7, Qt::Horizontal, tr("AGE"));                // 연령
+    sModel->setHeaderData(8, Qt::Horizontal, tr("GENDER"));             // 성별
+    sModel->setHeaderData(9, Qt::Horizontal, tr("JOINDATE"));           // 가입 날짜
 
+    // sModel에 담겨있는 정보를 searchTreeView 로 전달
     ui->searchTreeView->setModel(sModel);
 
 }
 
+//회원 관리 페이지의 데이터베이스
 void CustomerManage::dataSave()
 {
-    // DB 셋업
+    // 회원 DB 셋업, QSQLITE 형식으로 회원 접속 및 DB 명을 분리해 준다.
     QSqlDatabase ohcDB = QSqlDatabase::addDatabase("QSQLITE","customerConnection");
     ohcDB.setDatabaseName("customerDatabase.db");
 
     // 예외처리
     if(ohcDB.open())
     {
+        // 데이터베이스 ohcDB에 저장 및 호출되는 Sql Query 목록
         QSqlQuery cQuery(ohcDB);
+
+        // 회원 정보 테이블을 생성, Key 값인 cid 를 Primary Key 로 설정 및 항목별 자료형 선정
         cQuery.exec("CREATE TABLE IF NOT EXISTS customer"
                     "(cid INTEGER PRIMARY KEY, "
                     "name VARCHAR(20), "
@@ -72,12 +78,12 @@ void CustomerManage::dataSave()
                     "gender VARCHAR(50), "
                     "joinDate DATE);");
 
-        //       cModel = new QSqlTableModel();
-
+        // QSqlTableModel 형식으로 회원 정보 테이블을 저장
         cModel = new QSqlTableModel(this, ohcDB);
         cModel->setTable("customer");
         cModel->select();
 
+        // 테이블에서 사용하는 항목들의 머릿말 목록
         cModel->setHeaderData(0, Qt::Horizontal, tr("CID"));
         cModel->setHeaderData(1, Qt::Horizontal, tr("NAME"));
         cModel->setHeaderData(2, Qt::Horizontal, tr("PHONENUMBER"));
@@ -89,9 +95,12 @@ void CustomerManage::dataSave()
         cModel->setHeaderData(8, Qt::Horizontal, tr("GENDER"));
         cModel->setHeaderData(9, Qt::Horizontal, tr("JOINDATE"));
     }
+
+    // tableView 에서 보여줄 cModel 설정 및 항목 크기 조정
     ui->tableView->setModel(cModel);
     ui->tableView->resizeColumnsToContents();
 
+    // rowCount 순서에 따라 항목별 자료형과 데이터를 인덱스에 저장
     for(int i = 0; i < cModel->rowCount(); i++)
     {
         int Cid = cModel->data(cModel->index(i, 0)).toInt();
@@ -105,27 +114,37 @@ void CustomerManage::dataSave()
         QString gender = cModel->data(cModel->index(i, 8)).toString();
         QString joinDate = cModel->data(cModel->index(i, 9)).toString();
 
+        // purchaseManager 에서 사용될 Cid 값을 시그널 신호로 전달
         emit addedCustomer(Cid);
     }
 
 }
 
-//고객 등록
+// 회원 등록 버튼 클릭시 기능
 void CustomerManage::on_addPushButton_clicked()
 {
+    // 회원별 Key 값을 생성 및 부여, 항목별로 전달될 자료형을 선정한다
     int Cid = makeCid();
     QString name, phoneNumber, email, domain, address, favorite, gender, joinDate;
     int age;
+    // idLineEdit ui 에는 부여된 키값을 반환한다
     ui->idLineEdit->setText(QString::number(Cid));
 
+    // nameLineEdit ui 에 입력된 텍스트를 name 항목으로 반환
     name = ui->nameLineEdit->text();
+    // phoneNumberLineEdit ui 에 입력된 텍스트를 phoneNumber 항목으로 반환
     phoneNumber = ui->phoneNumberLineEdit->text();
+    // emailLineEdit ui 에 입력된 텍스트를 email 항목으로 반환
     email = ui->emailLineEdit->text();
+    // domainComboBox ui 에 입력된 텍스트를 domain 항목으로 반환
     domain = ui->domainComboBox->currentText();
+    // favoriteComboBox ui 에 입력된 텍스트를 favorite 항목으로 반환
     favorite = ui->favoriteComboBox->currentText();
+    // addressLineEdit ui 에 입력된 텍스트를 address 항목으로 반환
     address = ui->addressLineEdit->text();
+    // ageSpinBox ui 에 입력된 텍스트를 age 항목으로 반환
     age = ui->ageSpinBox->value();
-
+    // male, female Button ui 에서 선택한 정보를 gender 항목으로 반환
     if(ui->maleButton->isChecked())
     {
         gender = ui->maleButton->text();
@@ -134,14 +153,18 @@ void CustomerManage::on_addPushButton_clicked()
     {
         gender = ui->femaleButton->text();
     }
-
+    // dateEdit ui 에 입력된 텍스트를 joinDate 항목으로 반환
     joinDate = ui->dateEdit->date().toString();
 
+    // 회원 정보 데이터를 ohcDB 에 저장
     QSqlDatabase ohcDB = QSqlDatabase::database("customerConnection");
 
+    // name 항목에 길이가 존재할때 ohcDB 로부터 데이터를 읽어들인다
     if(ohcDB.isOpen() && name.length())
     {
+        // database 의 정보들을 cModel 로 반환하여 저장
         QSqlQuery cQuery(cModel->database());
+        // 항목의 갯수와 이름을 한번에 묶어서 cQuery에 담는다
         cQuery.prepare("INSERT INTO customer VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         cQuery.bindValue(0, Cid);
         cQuery.bindValue(1, name);
@@ -153,21 +176,24 @@ void CustomerManage::on_addPushButton_clicked()
         cQuery.bindValue(7, age);
         cQuery.bindValue(8, gender);
         cQuery.bindValue(9, joinDate);
-
         cQuery.exec();
+
+        // tableView 에서 보여줄 cModel 설정 및 항목 크기 조정
         cModel->select();
         ui->tableView->resizeColumnsToContents();
+        // purchaseManager 에서 사용될 Cid 값을 시그널 신호로 전달
         emit addedCustomer(Cid);
     }
 }
 
+// 수정 버튼 클릭 시 기능
 void CustomerManage::on_modifyPushButton_clicked()
 {
-
+    // 현재 tableView에 입력된 정보를 QModelIndex에 반환
     QModelIndex modelIndex = ui->tableView->currentIndex();
+    //
     if(modelIndex.isValid())
     {
-        //       int Cid=cModel->data(modelIndex.siblingAtColumn(0)).toInt();
         QString name, phoneNumber, email, domain, address, favorite, gender, joinDate;
         int age;
 
